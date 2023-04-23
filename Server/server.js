d380713cd9a0163161585f6951fd52361f9fce0e
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-04-23 20:37:14 +0200

    test connection

--- Server/server.js
+++ Server/server.js
@@ -1,30 +1,15 @@
-const net = require('net');
+var net = require('net');
+var readline = require('readline');
 
-const server = net.createServer(function (socket) {
-  socket.write('220 FTP server ready.\r\n');
+const server = net.createServer();
 
-  const clientAddress = socket.remoteAddress;
-  const clientPort = socket.remotePort;
-  console.log(`client connected with address ${clientAddress} using port ${clientPort}`);
+server.on('connection', socket => {
+      console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connectedd `);
+      socket.write('Welcome new client!\n');
 
-
-  socket.on('data', function (data) {
-    const command = data.toString().trim();
-    console.log('Received command:', command);
-      console.log("tets");
-    if (command === 'USER username') {
-      socket.write('331 Password required for username.\r\n');
-    } else if (command === 'PASS password') {
-      socket.write('230 User logged in.\r\n');
-    } else if (command === 'QUIT') {
-      socket.write('221 Goodbye.\r\n');
-      socket.end();
-    } else {
-      socket.write('500 Syntax error, command unrecognized.\r\n');
-    }
-  });
+      socket.on('data',data => {
+            console.log('data is:', data );
+      });
 });
 
-server.listen(21, function () {
-  console.log('FTP server listening on port 21');
-});
+server.listen(8000, () => console.log('Server Bound'));
