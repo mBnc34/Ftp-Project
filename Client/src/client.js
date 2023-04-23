aa9400f34d1db5bb179d472dfed6ead506456d71
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-04-23 04:14:53 +0200

    client initial config

--- Client/src/client.js
+++ Client/src/client.js
@@ -1,7 +1,18 @@
 var net = require('net');
-
+// https://github.com/bobchennan/FTP
 var readline = require('readline');
 const { stdin: input, stdout: output } = require('node:process');
+const fs = require('fs'); // manage file & directory
+const client = new net.Socket();
+
+// Connection of the client to the local server on his port 21 :
+client.connect(21, '127.0.0.1', function () {
+      console.log('Connected to FTP server.');
+    
+      client.write('USER username\r\n');
+      client.write('PASS password\r\n');
+      client.write('QUIT\r\n');
+    });
 
 
 var terminal = readline.createInterface({ input, output });
