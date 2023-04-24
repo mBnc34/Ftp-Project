096102e40fcd501fdbb7a1fb8c2a5775e43ccb05
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-04-24 16:36:12 +0200

    node module

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,18 @@
 var net = require('net');
 var readline = require('readline');
 
+const PORT = 21;
+const HOST = 'localhost';
 const server = net.createServer();
 
 server.on('connection', socket => {
-      console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connectedd `);
+      console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connected `);
       socket.write('Welcome new client!\n');
 
       socket.on('data',data => {
-            console.log('data is:', data );
+            console.log('data is:', data.toString() );
+            socket.write(`le server a recu les donnees : ${data.toString()}\r\n`)
       });
 });
 
-server.listen(8000, () => console.log('Server Bound'));
+server.listen(PORT, HOST, () => console.log('Server Bound'));
