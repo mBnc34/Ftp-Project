eb19104993af504c455f0e079bd84f5d8fb29cf6
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-04-27 03:10:29 +0200

    change

--- Server/commands/user.js
+++ Server/commands/user.js
@@ -1,11 +1,45 @@
-var command = require('../lib/command');
-var server = require('../server');
+const { add } = require('../command');
+const users = require('../Users/users.json');
+const {user, connectionSocket, server} = require('../server');
 
 const name = 'USER';
 const helpText = 'USER <sp> <username>';
 const description = 'To authenticate';
 
-// command.add(name, helpText){
+function userFunction (username) {
+      if(connectionSocket != null){
+             // console.log(message);
+      if (user) {
+            // message d'erreur : already connected
+            connectionSocket.write('already connected')
+            console.log("deja connecté");
+            return;
+      };
+      // else
 
-      // server.user = name;
-// };
+      // verifie que le user existe: 
+      if (!existUser(username)){
+            // message d'erreur : le user n'existe pas
+            // peut-etre faire apres pass.
+            console.log("user doesnt exist");
+            connectionSocket.write("user doesn't exist");
+            return;
+      }
+
+      // else
+      console.log("ici");
+      connectionSocket.write("j'attends pass;")
+      // message pour dire d'ecrire PASS + password
+      // serveur en ecoute de data
+      // appelé passFunction
+      }
+     console.log("pas de socket, pas de connection");
+}
+
+function existUser(username){
+      return Object.keys(users).includes(username);
+}
+
+add(name, helpText, description, userFunction);
+
+module.exports = {userFunction};
--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,39 @@
 var net = require('net');
 var readline = require('readline');
+const { userFunction } = require('./commands/user')
 
 const PORT = 21;
 const HOST = 'localhost';
 const server = net.createServer();
-export var user = "";
+var user = "";
+var connectionSocket;
+
+module.exports = {
+      user,
+      // connectionSocket,
+      server
+};
+
+
 
 server.on('connection', socket => {
       console.log(`Client ${socket.remoteAddress}:${socket.remotePort} connected `);
       socket.write('Welcome new client!\n');
-
-      socket.on('data',data => {
-            console.log('data is:', data.toString() );
+      connectionSocket = socket;
+      socket.on('data', data => {
+            console.log('data is:', data.toString());
             socket.write(`le server a recu les donnees : ${data.toString()}\r\n`)
+            if (data.toString().includes("USER")) {
+                  const [command, name] = data.toString().split(' ');
+                  console.log("user bien reco");
+                  console.log(name);
+                  // socket.write(`ecrivez le nom d'utilisateur\r\n`);
+                  // socket.once('data', data => {
+                  //       console.log(data);
+                  //       userFunction(data);
+                  // })
+                  userFunction(socket, name);
+            }
       });
 });
 
